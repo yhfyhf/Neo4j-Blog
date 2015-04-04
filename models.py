9d4a52d100d5dbdf06bd0c47160654e423aa27da
yhf <xdhfying@gmail.com>
2015-04-04 16:55:11 +0800

    first commit

---
+++ models.py
@@ -0,0 +1,160 @@
+from py2neo import Graph, Node, Relationship, authenticate
+from passlib.hash import bcrypt
+from datetime import datetime
+import os
+import uuid
+
+
+url = os.environ.get('NEO4J_URL', 'http://localhost:7474')
+username = os.environ.get('NEO4J_USERNAME')
+password = os.environ.get('NEO4J_PASSWORD')
+
+
+if username and password:
+    authenticate(url.strip('http://'), username, password)
+
+graph = Graph(url + '/db/data/')
+
+
+class User:
+    def __init__(self, username):
+        self.username = username
+
+    def find(self):
+        """
+        Find a noe in the database with label User and the given username,
+        return a py2neo.Node object.
+        """
+        user = graph.find_one("User", "username", self.username)
+        return user
+
+    def set_password(self, password):
+        self.password = bcrypt.encrypt(password)
+        return self
+
+    def verify_password(self, password):
+        user = self.find()
+        if user:
+            return bcrypt.verify(password, user['password'])
+        else:
+            return False
+
+    def register(self):
+        if not self.find():
+            user = Node("User", username=self.username, password=self.password)
+            graph.create(user)
+            return True
+        else:
+            return False
+
+    def add_post(self, title, tags, text):
+        user = self.find()
+        post = Node(
+            "Post",
+            id=str(uuid.uuid4()),
+            title=title,
+            text=text,
+            timestamp=timestamp(),
+            date=date(),
+        )
+        # create a (:User)-[:PUBLISHED]->(:Post) relationship
+        rel = Relationship(user, "PUBLISHED", post)
+        graph.create(rel)
+
+        tags = [tag.strip() for tag in tags.lower().split(',')]
+        for t in tags:
+            # (:Tag)-[:TAGGED]->(:Post)
+            tag = graph.merge_one("Tag", "name", t)
+            rel = Relationship(tag, "TAGGED", post)
+            graph.create(rel)
+
+    def like_post(self, post_id):
+        user = self.find()
+        post = graph.find_one("Post", "id", post_id)
+        graph.create_unique(Relationship(user, "LIKED", post))
+
+    def get_similar_users(self):
+        # Find three users who are most similar to the logged-in user
+        # based on tags they've both blogged about.
+        query = """
+            MATCH (you:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+                  (they:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+            WHERE you.username = {username} AND you <> they
+            WITH they, 
+                 COLLECT(DISTINCT tag.name) AS tags, 
+                 COUNT(DISTINCT tag) AS len
+            ORDER BY len DESC LIMIT 3
+            RETURN they.username AS similar_user, tags
+        """
+
+        similar = graph.cypher.execute(query, username=self.username)
+        return similar
+
+    def get_commonality_of_user(self, username):
+        # Find how many of the logged-in user's posts the other user
+        # has liked and which tags they've both blogged about.
+        query = """
+            MATCH (they:User {username:{they}}),
+                  (you:User {username:{you}})
+            OPTIONAL MATCH (they)-[:LIKED]->(post:Post)<-[:PUBLISHED]-(you)
+            OPTIONAL MATCH (they)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+                           (you)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+            RETURN COUNT(DISTINCT post) AS likes, 
+                   COLLECT(DISTINCT tag.name) AS tags
+        """
+
+        result = graph.cypher.execute(query,
+                                      they=username,
+                                      you=self.username)
+
+        result = result[0]
+        common = dict()
+        common['likes'] = result.likes
+        common['tags'] = result.tags if len(result.tags) > 0 else None
+        return common
+
+
+def get_users_recent_posts(username):
+    query = """
+        MATCH (:User {username:{username}})-[:PUBLISHED]->(post:Post),
+              (tag:Tag)-[:TAGGED]->(post)
+        RETURN post.id AS id,
+               post.date AS date,
+               post.timestamp AS timestamp,
+               post.title AS title,
+               post.text AS text,
+               COLLECT(tag.name) AS tags
+        ORDER BY timestamp DESC
+        LIMIT 5
+    """
+
+    posts = graph.cypher.execute(query, username=username)
+    return posts
+
+def get_todays_recent_posts():
+    query = """
+        MATCH (post:Post {date: {today}}),
+              (user:User)-[:PUBLISHED]->(post),
+              (tag:Tag)-[:TAGGED]->(post)
+        RETURN user.username AS username,
+               post.id AS id,
+               post.date AS date,
+               post.timestamp AS timestamp,
+               post.title AS title,
+               post.text AS text,
+               COLLECT(tag.name) AS tags
+        ORDER BY timestamp DESC
+        LIMIT 5
+    """
+
+    posts = graph.cypher.execute(query, today = date())
+    return posts
+
+def timestamp():
+    epoch = datetime.utcfromtimestamp(0)
+    now = datetime.now()
+    delta = now - epoch
+    return delta.total_seconds()
+
+def date():
+    return datetime.now().strftime('%F')
---
+++ views.py
@@ -0,0 +1,108 @@
+from models import User, get_users_recent_posts, get_todays_recent_posts
+from flask import Flask, request, session, redirect, url_for,\
+	abort, render_template, flash
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    posts = get_todays_recent_posts()
+    return render_template('index.html', posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	error = None
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		if len(username) < 1:
+			error = 'Your username must be at least one character.'
+		elif len(password) < 5:
+			error = 'Your password must be at least 5 characters.'
+		elif not User(username).set_password(password).register():
+			error = 'A user with that username already exists.'
+		else:
+			flash('Successfully registered. Please login.')
+			return redirect(url_for('login'))
+
+	return render_template('register.html', error=error)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		user = User(username)
+
+		if not user.verify_password(password):
+			error = 'Invalid login'
+		else:
+			session['username'] = user.username
+			flash('Logged in.')
+			return redirect(url_for('index'))
+
+	return render_template('login.html', error=error)
+
+@app.route('/add_post', methods=['POST'])
+def add_post():
+	user = User(session['username'])
+	title = request.form['title']
+	tags = request.form['tags']
+	text = request.form['text']
+
+	if title == '':
+		abort(400, 'You must give your post a title.')
+	if tags == '':
+		abort(400, 'You must give your post at least one tag.')
+	if text == '':
+		abort(400, 'You must give your post a text body.')
+
+	user.add_post(title, tags, text)
+	return redirect(url_for('index'))
+
+@app.route('/like_post/<post_id>', methods=['GET'])
+def like_post(post_id):
+	username = session.get('username')
+	if not username:
+		abort(400, 'You must be logged in to like a post.')
+
+	user = User(username)
+	user.like_post(post_id)
+	flash('Liked post.')
+	return redirect(request.referrer)
+
+@app.route('/profile/<username>', methods=['GET'])
+def profile(username):
+    posts = get_users_recent_posts(username)
+
+    similar = []
+    common = []
+
+    viewer_username = session.get('username')
+    if viewer_username:
+        viewer = User(viewer_username)
+        # If they're visiting their own profile, show similar users.
+        if viewer.username == username:
+            similar = viewer.get_similar_users()
+        # If they're visiting another user's profile, show what they
+        # have in common with that user.
+        else:
+            common = viewer.get_commonality_of_user(username)
+
+    return render_template(
+        'profile.html',
+        username=username,
+        posts=posts,
+        similar=similar,
+        common=common
+    )
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('username', None)
+    flash('Logged out.')
+    return redirect(url_for('index'))
